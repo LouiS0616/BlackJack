640e8702a008528f7bcc4b7f65e90801a3c4fbd1
Loui Sakaki <e1352207@outlook.jp>
2018-05-14 12:23:42 +0900

    Remove error temporary by comment out.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import cards.ordered.Deck;
-
 public class Main {
     public static void main(String[] args) {
-        Deck deck = Deck.makeDeck();
-        deck.printCards();
+        //Deck deck;
     }
 }
